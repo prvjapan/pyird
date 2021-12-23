e03e4c2d33ab47b452d0b5d2605b9471f124c534
Hajime Kawahara <divrot@gmail.com>
2021-12-23 22:34:28 +0900

    added test for iraf trace

--- src/pyird/io/iraf_trace.py
+++ src/pyird/io/iraf_trace.py
@@ -36,29 +36,30 @@ def read_trace_file(filename):
                     order.append(curvepar[1])
                     xmin.append(curvepar[2])
                     xmax.append(curvepar[3])
-                    coeff.append(curvepar[4:])
+                    coeffval=[float(s) for s in curvepar[4:]]
+                    coeff.append(coeffval)
                     read_curve=False
 
                 
                 norder=norder+1
-                y0.append(arr[4])
+                y0.append(float(arr[4]))
             elif arr[0]=="curve":
                 read_curve=True
                 curvepar=[]
             elif read_curve and arr[0]!="#":
                 curvepar.append(float(arr[0]))
                 
-    interp_function.append(curvepar[0])
-    order.append(curvepar[1])
-    xmin.append(curvepar[2])
-    xmax.append(curvepar[3])
-    coeff.append(curvepar[4:])
+    interp_function.append(float(curvepar[0]))
+    order.append(float(curvepar[1]))
+    xmin.append(float(curvepar[2]))
+    xmax.append(float(curvepar[3]))
+    coeffval=[float(s) for s in curvepar[4:]]
+    coeff.append(coeffval)
 
     interp_function=np.array(interp_function,dtype=int)
     order=np.array(order,dtype=int)
     xmin=np.array(xmin,dtype=int)
     xmax=np.array(xmax,dtype=int)
-
     
     return y0, interp_function, order, xmin, xmax, coeff
 
