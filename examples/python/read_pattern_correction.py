8e42571508decf93f556334232c57bb62bc26aae
Hajime Kawahara <divrot@gmail.com>
2021-12-25 08:34:57 +0900

    masked read pattern correction

--- examples/python/read_pattern_correction.py
+++ examples/python/read_pattern_correction.py
@@ -1,43 +1,94 @@
 import numpy as np
+import pkg_resources
 from pyird.utils import irdstream
 import pathlib
 import matplotlib.pyplot as plt
 from pyird.image.channel import image_to_channel_cube, channel_cube_to_image, eopixel_split, eopixel_combine
 from pyird.image.bias import bias_subtract
+from pyird.image.trace_function import trace_legendre
+from pyird.io.iraf_trace import read_trace_file
 
-datadir=pathlib.Path("/home/kawahara/pyird/data/dark/")
-anadir=pathlib.Path("/home/kawahara/pyird/data/dark/")
-dark=irdstream.Stream2D("targets",datadir,anadir)
-dark.fitsid=[41018]
+mode="faint"
+if mode=="dark":
+    datadir=pathlib.Path("/home/kawahara/pyird/data/dark/")
+    anadir=pathlib.Path("/home/kawahara/pyird/data/dark/")
+    target=irdstream.Stream2D("targets",datadir,anadir)
+    target.fitsid=[41018]
+elif mode=="faint":
+    datadir=pathlib.Path("/home/kawahara/pyird/data/samples/REACH/")
+    anadir=pathlib.Path("/home/kawahara/pyird/data/samples/REACH/")
+    target=irdstream.Stream2D("targets",datadir,anadir)
+    #target.fitsid=[47103]
+    target.fitsid=[47077]
+else:
+    print("no mode")
+    import sys
+    sys.exit()
 
+# Load an image
 import astropy.io.fits as pyf
-for datapath in dark.rawpath:
+for datapath in target.rawpath:
     im = pyf.open(str(datapath))[0].data
 
-channel_cube=image_to_channel_cube(im,revert=True)
-#bs_channel_cube, bias=bias_subtract(channel_cube)
-#eotensor=eopixel_split(bs_channel_cube)
+#aperture
+if mode=="faint":
+    pathC=(pkg_resources.resource_filename('pyird', "data/samples/aprefC"))
+    path_c=(pkg_resources.resource_filename('pyird', "data/samples/apref_c"))
+    y0, interp_function, xmin, xmax, coeff=read_trace_file([pathC,path_c])
 
-eotensor=eopixel_split(channel_cube)
+    #trace
+    x=[]
+    for i in range(len(y0)):
+        x.append(list(range(xmin[i],xmax[i]+1)))
+    tl=trace_legendre(x, y0, xmin, xmax, coeff)
 
-#### CODE
-_,nchan,xsize,ysize=np.shape(eotensor)
-channel_median_offset=np.median(eotensor,axis=(2,3))
-xprofile_offset_subtracted=np.median(eotensor,axis=3)-channel_median_offset[:,:,np.newaxis]
-yprofile_offset_subtracted=np.median(eotensor,axis=2)-channel_median_offset[:,:,np.newaxis]
+    import tqdm
+    mask=np.zeros_like(im,dtype=bool)    
+    width=2
+    nx,ny=np.shape(im)
+    for i in tqdm.tqdm(range(len(y0))):
+        tl_tmp=np.array(tl[i],dtype=int)
+        for j,ix in enumerate(x[i]):
+            iys=np.max([0,tl_tmp[j]-width])
+            iye=np.min([ny,tl_tmp[j]+width+2])
+            mask[ix,iys:iye]=True
+            
+    calim=np.copy(im[::-1,::-1])
+    calim[mask]=np.nan
+    calim=calim[::-1,::-1]
+    
+#############################################################
+# REMOVAL CODE
 
-#median as model
-xprofile_offset_subtracted_model=np.median(xprofile_offset_subtracted,axis=1)
-yprofile_offset_subtracted_model=np.median(yprofile_offset_subtracted,axis=1)
+
+cal_channel_cube=image_to_channel_cube(calim,revert=True)
+cal_eotensor=eopixel_split(cal_channel_cube)
+_,nchan,xsize,ysize=np.shape(cal_eotensor)
+channel_median_offset=np.nanmedian(cal_eotensor,axis=(2,3))
+xprofile_offset_subtracted=np.nanmedian(cal_eotensor,axis=3)-channel_median_offset[:,:,np.newaxis]
+yprofile_offset_subtracted=np.nanmedian(cal_eotensor,axis=2)-channel_median_offset[:,:,np.newaxis]
+
+#median as model--------------------
+xprofile_offset_subtracted_model=np.nanmedian(xprofile_offset_subtracted,axis=1)
+yprofile_offset_subtracted_model=np.nanmedian(yprofile_offset_subtracted,axis=1)
 
 image_pattern_model=\
     xprofile_offset_subtracted_model[:,np.newaxis,:,np.newaxis]\
     +yprofile_offset_subtracted_model[:,np.newaxis,np.newaxis,:]\
     +channel_median_offset[:,:,np.newaxis,np.newaxis]
+#-----------------------------------
 
-#print(np.shape(image_pattern_model))
-#import sys
-#sys.exit()
+#original image
+channel_cube=image_to_channel_cube(im,revert=True)
+eotensor=eopixel_split(channel_cube)
+
+
+eotensor_corrected = eotensor - image_pattern_model
+channel_cube_corrected=eopixel_combine(eotensor_corrected)
+corrected_im=channel_cube_to_image(channel_cube_corrected)
+#############################################################
+
+
 
 fig=plt.figure(figsize=(10,9))
 ax=fig.add_subplot(311)
@@ -71,15 +122,15 @@ plt.ylim(-3,3)
 plt.xlabel("pixel")
 plt.show()
 
-eotensor_corrected = eotensor - image_pattern_model
-
-####
-channel_cube_corrected=eopixel_combine(eotensor_corrected)
-corrected_im=channel_cube_to_image(channel_cube_corrected)
 
 fig=plt.figure(figsize=(8,4))
 ax1=fig.add_subplot(121)
-cc=ax1.imshow(im,vmin=-15.0,vmax=-8.0)
+if mode=="dark":
+    vmax=-8.0;vmin=-15.0
+elif mode=="faint":
+    vmax=10.0;vmin=-15.0
+
+cc=ax1.imshow(im,vmin=vmin,vmax=vmax)
 plt.colorbar(cc,shrink=0.55)
 ax1.set_title("Raw dark image")
 ax2=fig.add_subplot(122)
