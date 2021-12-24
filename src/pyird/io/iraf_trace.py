c70d639e45539917be07f5fa7d678bea50fe54e9
Hajime Kawahara <divrot@gmail.com>
2021-12-24 09:55:43 +0900

    trace

--- src/pyird/io/iraf_trace.py
+++ src/pyird/io/iraf_trace.py
@@ -9,7 +9,7 @@ def read_trace_file(filename):
        trace file
 
     Returns:
-        y0, interp_function, order, xmin, xmax, coeff
+        y0, interp_function, xmin, xmax, coeff
 
     """
     
@@ -20,7 +20,6 @@ def read_trace_file(filename):
     y0=[]
     read_curve=False
     interp_function=[]
-    order=[]
     curvepar=[]
     xmin=[]
     xmax=[]
@@ -33,13 +32,12 @@ def read_trace_file(filename):
                 #reset read_curve
                 if len(curvepar)>0:
                     interp_function.append(curvepar[0])
-                    order.append(curvepar[1])
+                    nlegendreorder=int(curvepar[1])
                     xmin.append(curvepar[2])
                     xmax.append(curvepar[3])
-                    coeffval=[float(s) for s in curvepar[4:]]
+                    coeffval=[float(s) for s in curvepar[4:4+nlegendreorder]]
                     coeff.append(coeffval)
                     read_curve=False
-
                 
                 norder=norder+1
                 y0.append(float(arr[4]))
@@ -50,18 +48,16 @@ def read_trace_file(filename):
                 curvepar.append(float(arr[0]))
                 
     interp_function.append(float(curvepar[0]))
-    order.append(float(curvepar[1]))
     xmin.append(float(curvepar[2]))
     xmax.append(float(curvepar[3]))
-    coeffval=[float(s) for s in curvepar[4:]]
+    nlegendreorder=int(curvepar[1])
+    coeffval=[float(s) for s in curvepar[4:4+nlegendreorder]]
     coeff.append(coeffval)
-
     interp_function=np.array(interp_function,dtype=int)
-    order=np.array(order,dtype=int)
     xmin=np.array(xmin,dtype=int)
     xmax=np.array(xmax,dtype=int)
     
-    return y0, interp_function, order, xmin, xmax, coeff
+    return y0, interp_function, xmin, xmax, coeff
 
 
 if __name__=="__main__":
--- tests/io/test_iraf_trace.py
+++ tests/io/test_iraf_trace.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def test_read_trace_file():
     path=(pkg_resources.resource_filename('pyird', "data/samples/aprefA"))
-    y0, interp_function, order, xmin, xmax, coeff=read_trace_file(path)
+    y0, interp_function, xmin, xmax, coeff=read_trace_file(path)
     assert np.sum(y0)==13517.2013
 
 if __name__=="__main__":
