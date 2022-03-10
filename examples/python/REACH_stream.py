02d1ee4ac6f9068004697875ce1046b2f40ed5d4
Hajime Kawahara <divrot@gmail.com>
2022-03-10 14:32:04 +0900

    remove unc imports

--- examples/python/REACH_stream.py
+++ examples/python/REACH_stream.py
@@ -1,16 +1,7 @@
-from pyird.spec.rsdmat import multiorder_to_rsd
-from pyird.image.oned_extract import flatten
-from pyird.plot.detector import corrected_detector
-from pyird.image.pattern_model import median_XY_profile
-from pyird.image.trace_function import trace_legendre
-from pyird.image.mask import trace
-from pyird.io.iraf_trace import read_trace_file
 import numpy as np
 import pkg_resources
 from pyird.utils import irdstream
 import pathlib
-import matplotlib.pyplot as plt
-from pyird.image.channel import image_to_channel_cube, channel_cube_to_image, eopixel_split, eopixel_combine
 from pyird.image.bias import bias_subtract_image
 from pyird.image.hotpix import identify_hotpix
 import astropy.io.fits as pyf
@@ -28,15 +19,17 @@ hotpix_mask, obj = identify_hotpix(im_subbias)
 # Load data
 datadir = pathlib.Path('/home/kawahara/pyird/data/samples/REACH/')
 anadir = pathlib.Path('/home/kawahara/pyird/data/samples/REACH/')
-target = irdstream.Stream2D('targets', datadir, anadir, fitsid=[47078,47080,47082])
-target.info = True #show detailed info
+target = irdstream.Stream2D(
+    'targets', datadir, anadir, fitsid=[47078, 47080, 47082])
+target.info = True  # show detailed info
 
-#clean pattern
+# clean pattern
 pathC = (pkg_resources.resource_filename('pyird', 'data/samples/aprefC'))
 path_c = (pkg_resources.resource_filename('pyird', 'data/samples/apref_c'))
-target.clean_pattern(extin="",extout="_cp",trace_path_list=[pathC,path_c],hotpix_mask=hotpix_mask)
+target.clean_pattern(extin='', extout='_cp', trace_path_list=[
+                     pathC, path_c], hotpix_mask=hotpix_mask)
 
-#flatten
-path_trace_flatten = (pkg_resources.resource_filename('pyird', 'data/samples/aprefB'))
+# flatten
+path_trace_flatten = (pkg_resources.resource_filename(
+    'pyird', 'data/samples/aprefB'))
 target.flatten(path_trace_flatten)
-
