66859746de7fcb771a5df53e1c936611aec67cdf
Hajime Kawahara <divrot@gmail.com>
2022-03-17 21:34:50 +0900

    test passed

--- examples/python/REACH_stream.py
+++ examples/python/REACH_stream.py
@@ -42,10 +42,11 @@ target.flatten(path_trace_flatten)
 datadir = pathlib.Path('/home/kawahara/pyird/data/samples/REACH/')
 anadir = pathlib.Path('/home/kawahara/pyird/data/samples/REACH/')
 
+#wavelength calibration
 pathA = (pkg_resources.resource_filename('pyird', 'data/samples/aprefA'))
 thar=irdstream.Stream2D("thar",datadir,anadir,rawtag="IRDBD000",fitsid=list(range(15480,15530))) 
 thar.clean_pattern(extin='', extout='_cp', trace_path_list=[pathC, path_c], hotpix_mask=hotpix_mask)
 wavsol, data=thar.calibrate_wavlength(pathA)
-#thar.flatten(path_trace_flatten)
+
 
 
