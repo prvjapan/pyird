cb0ce19ad08a78af5be201c05dcb7fb81f95f716
Yui Kasagi <yui.kasagi@grad.nao.ac.jp>
2022-04-25 09:37:51 +0900

    IRD stream (ver.1; before wavelength allocation)

--- src/pyird/utils/aperture.py
+++ src/pyird/utils/aperture.py
@@ -9,17 +9,17 @@ __all__ = ['TraceAperture']
 class TraceAperture(object):
     """aperture instance for trace class
 
-    """    
+    """
     def __init__(self, trace_function, y0, xmin, xmax, coeff):
         """initialization
-        
+
         Args:
             trace_function:  trace function used in interpolation
             y0:  y0
             xmin:  xmin
             xmax:  xmax
             coeff:  polinomial coeff
-        
+
         """
         self.trace_function = trace_function
         self.y0 = y0
@@ -29,11 +29,16 @@ class TraceAperture(object):
 
     def mask(self):
         """mask image
-        
+
         Returns:
             mask image
 
         """
         return trace(self.trace_function, self.y0, self.xmin, self.xmax, self.coeff)
 
-    
+    def mmf2(self):
+        self.y0 = self.y0[::2]
+        self.xmin = self.xmin[::2]
+        self.xmax = self.xmax[::2]
+        self.coeff = self.coeff[::2]
+        return self
