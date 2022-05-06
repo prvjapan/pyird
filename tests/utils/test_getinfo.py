45a182a9848e4335139ac53daf948e589822a08b
Hajime Kawahara <divrot@gmail.com>
2022-05-06 14:21:11 +0900

    put A in HR8799

--- tests/utils/test_getinfo.py
+++ tests/utils/test_getinfo.py
@@ -3,10 +3,10 @@ from pyird.utils.getinfo import get_radec
 
 
 def test_get_radec():
-    ra, dec = get_radec('HR8799')
+    ra, dec = get_radec('HR8799 A')
     print(ra, dec)
-    delta = ((ra - 346.86964833333326)**2+(dec - 21.134250555555553)**2)
-    assert delta == 0.0
+    delta = ((ra - 346.86964875)**2+(dec - 21.134252777777778)**2)
+    assert pytest.approx(delta) == 0.0
 
 
 if __name__ == '__main__':
