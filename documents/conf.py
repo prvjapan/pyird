6ffb129a8f3d48dfdfdb7b06790525b93330e3fd
Hajime Kawahara <divrot@gmail.com>
2022-10-14 17:20:27 +0900

    version update

--- documents/conf.py
+++ documents/conf.py
@@ -23,7 +23,7 @@ copyright = '2021-2022, Hajime Kawahara, Yui Kasagi, Masayuki Kuzuhara'
 author = 'Hajime Kawahara, Yui Kasagi, Masayuki Kuzuhara'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             ),
             'write_to_template': '__version__ = "{version}"\n',
         },
-        version='0.1.0',
+        version='0.2.0',
         author=find_meta('author'),
         author_email=find_meta('email'),
         maintainer=find_meta('author'),
