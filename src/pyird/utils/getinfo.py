83c444667521b9b4026cd0cb31c4043b9158c210
Yui Kasagi <yui.kasagi@grad.nao.ac.jp>
2024-10-20 16:13:42 +0900

    fix getinfo.py

--- src/pyird/utils/getinfo.py
+++ src/pyird/utils/getinfo.py
@@ -22,13 +22,17 @@ def get_radec(name):
     if parse(astroquery.__version__) <= parse("0.4.7"):
         Simbad.add_votable_fields(
             'sp', 'flux(V)', 'flux(R)', 'flux(J)', 'flux(H)', 'flux(K)')
+        result_table = Simbad.query_object(name)
+        namex = result_table['MAIN_ID'][0]  # .decode('utf-8')
+        ra = result_table['RA'][0]
+        dec = result_table['DEC'][0]
     else:
         Simbad.add_votable_fields('sp', 'flux')
+        result_table = Simbad.query_object(name)
+        namex = result_table['main_id'][0]  # .decode('utf-8')
+        ra = result_table['ra'][0]
+        dec = result_table['dec'][0]
 
-    result_table = Simbad.query_object(name)
-    namex = result_table['MAIN_ID'][0]  # .decode('utf-8')
-    ra = result_table['RA'][0]
-    dec = result_table['DEC'][0]
     c = SkyCoord(str(ra)+' '+str(dec), unit=(u.hourangle, u.deg))
     ra = c.ra.degree
     dec = c.dec.degree
